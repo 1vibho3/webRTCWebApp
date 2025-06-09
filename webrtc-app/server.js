d3d054f62a08406dd0e843ae2ab8cd7f39b47a75
vibho <1vibho3@gmail.com>
2025-06-09 12:53:18 -0500

    Add a server

---
+++ webrtc-app/server.js
@@ -0,0 +1,16 @@
+// server.js
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Serve the built React app
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('*', (_, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
